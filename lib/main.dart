77ad4767847eac3e04b3a283a6b1def82c446b5a
ANGEL_MANUEL_GOMEZ_HERNANDEZ <a.21308051281222@cbtis128.edu.mx>
2024-02-29 17:10:02 +0000

    actualizando

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,90 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AppTabBar());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class AppTabBar extends StatelessWidget {
+  const AppTabBar({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      debugShowCheckedModeBanner: false,
+      title: '',
       theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MiPaginaInicial(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MiPaginaInicial extends StatefulWidget {
+  const MiPaginaInicial({Key? key}) : super(key: key);
 
   @override
+  State<MiPaginaInicial> createState() => _MipaginaInicialState();
+}
+
+class _MipaginaInicialState extends State<MiPaginaInicial> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("TabBar Angel Gomez"),
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                text: "Usuario",
+                icon: Icon(Icons.person),
+              ),
+              Tab(
+                text: "Compra",
+                icon: Icon(Icons.shopping_cart),
+              ),
+              Tab(
+                text: "Fill",
+                icon: Icon(Icons.fire_truck),
+              ),
+              Tab(
+                text: "Jefe",
+                icon: Icon(Icons.people_alt_outlined),
+              )
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: const <Widget>[
+            Center(
+              child: Text("Opcion 1",
+                  style: TextStyle(
+                      fontSize: 50,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold)),
+            ),
+            Center(
+              child: Text("Opcion 2",
+                  style: TextStyle(
+                      fontSize: 50,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold)),
+            ),
+            Center(
+              child: Text("Opcion 3",
+                  style: TextStyle(
+                      fontSize: 50,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold)),
+            ),
+            Center(
+              child: Text("Opcion 4",
+                  style: TextStyle(
+                      fontSize: 50,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold)),
+            ),
+          ],
         ),
       ),
     );
